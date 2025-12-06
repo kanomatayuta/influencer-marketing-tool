b0b4b7504f7875b6871c5bf26ab1ea6f71d400ea
kanomatayuta <skr7110@icloud.com>
2025-12-07 01:28:30 +0900

    fix: allow dynamic property access on request types to support Express properties

--- apps/api/src/types/request.ts
+++ apps/api/src/types/request.ts
@@ -1,19 +1,23 @@
 import { Request } from 'express';
 import { JWTPayload } from '../utils/jwt';
 
-export interface AuthRequest extends Request<any, any, any, any> {
+// Express Request の任意のプロパティにアクセス可能にする
+export interface AuthRequest extends Request {
   user?: JWTPayload;
   requestId?: string;
   file?: Express.Multer.File;
+  [key: string]: any;
 }
 
-export interface AuthenticatedRequest extends Request<any, any, any, any> {
+export interface AuthenticatedRequest extends Request {
   user?: JWTPayload;
   requestId?: string;
   file?: Express.Multer.File;
+  [key: string]: any;
 }
 
-export interface RequestWithId extends Request<any, any, any, any> {
+export interface RequestWithId extends Request {
   id?: string;
   requestId?: string;
+  [key: string]: any;
 }
